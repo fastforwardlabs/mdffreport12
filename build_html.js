f10b0b5cf04bc8fac15407036eaae3f07f0940e4
Grant Custer <grantcuster@gmail.com>
2020-02-11 17:20:07 -0500

    analytics issue again

--- build_html.js
+++ build_html.js
@@ -586,14 +586,15 @@ function wrap(content) {
           ${content}
         </div>
       </body>
-      <!-- Global site tag (gtag.js) - Google Analytics -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-157475426-2"></script>
+      <!-- Google Analytics -->
       <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-        gtag('config', 'UA-157475426-2');
+        ga('create', 'UA-157475426-2', 'auto');
+        ga('send', 'pageview');
 
         window.addEventListener('load', function() {
           document.getElementById('report-pdf-download').addEventListener('click', function() {
@@ -603,7 +604,9 @@ function wrap(content) {
             });
           });
         })
+
       </script>
+      <!-- End Google Analytics -->
     </html>
   `
 }
