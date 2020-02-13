027a83ed20c0e702425d1cdac57560cc37940cbd
grant <grantcuster@gmail.com>
2020-02-13 13:16:53 -0500

    add favicon

--- build_html.js
+++ build_html.js
@@ -572,7 +572,7 @@ function makeHead() {
     <meta name="twitter:card" content="summary_large_image" />
     
     <meta name="viewport" content="width=device-width" />
-    <link rel="icon" type="image/x-icon" href="/static/images/favicon.png" />
+    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
     
     ${makeStyle()}
     ${makeJS()}
